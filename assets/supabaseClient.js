65c027a14edad7281da59e4991913278cf5facd0
brutus-the-homeschooler <156854267+brutus-the-homeschooler@users.noreply.github.com>
2025-08-25 14:40:28 -0400

    Update supabaseClient.js

--- assets/supabaseClient.js
+++ assets/supabaseClient.js
@@ -1,6 +1,6 @@
 
-const SUPABASE_URL = "https://YOUR-PROJECT-REF.supabase.co"; // <-- replace
-const SUPABASE_ANON_KEY = "YOUR-ANON-PUBLIC-KEY";             // <-- replace (safe with RLS)
+const SUPABASE_URL = "https://sbrknvtpeexfylbxyhkp.supabase.co"; // <-- replace
+const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InNicmtudnRwZWV4ZnlsYnh5aGtwIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTYxNDQwOTcsImV4cCI6MjA3MTcyMDA5N30.5nQAjfwch_G6rvHrpKqHnNJKhFfl9sIwgXjOO0FkRIY";             // <-- replace (safe with RLS)
 
 const supabase = window.supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
 
