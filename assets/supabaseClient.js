a31a46071b1e7d45d414533f8a5a3968ba21ae22
brutus-the-homeschooler <156854267+brutus-the-homeschooler@users.noreply.github.com>
2025-09-06 21:30:00 -0400

    Update supabaseClient.js

--- assets/supabaseClient.js
+++ assets/supabaseClient.js
@@ -1,6 +1,6 @@
 if (!window.sb) {
   const SUPABASE_URL = "https://sbrknvtpeexfylbxyhkp.supabase.co";
-  const SUPABASE_ANON_KEY = "eyJh...";
+  const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InNicmtudnRwZWV4ZnlsYnh5aGtwIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTYxNDQwOTcsImV4cCI6MjA3MTcyMDA5N30.5nQAjfwch_G6rvHrpKqHnNJKhFfl9sIwgXjOO0FkRIY";
 
   const supabase = window.supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
 
